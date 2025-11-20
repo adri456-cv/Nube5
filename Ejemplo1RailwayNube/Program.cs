d163908df85670325f9439e661e20e0913dc7386
adri456-cv <adrianacondori679@gmail.com>
2025-11-20 01:00:03 -0400

    Fix: Eliminada configuracion forzada de HTTPS y puerto para despliegue en Railway

--- Ejemplo1RailwayNube/Program.cs
+++ Ejemplo1RailwayNube/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.WebHost.UseUrls("https://localhost:7130");
+//builder.WebHost.UseUrls("https://localhost:7130");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,7 +13,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
