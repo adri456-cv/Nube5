51169a97641a190b638c7961a58d66eab943dccf
adri456-cv <adrianacondori679@gmail.com>
2025-11-20 00:31:51 -0400

    Prueba5

---
+++ Ejemplo1RailwayNube/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.WebHost.UseUrls("https://localhost:7130");
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
